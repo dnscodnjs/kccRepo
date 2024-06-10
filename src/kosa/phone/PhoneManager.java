63fe55a15d57d81f9e597c13cb76564c22ce8bdc
Your Name <you@example.com>
2024-06-10 15:23:07 +0900

    PhoneManger.java list->stream 변경

--- src/kosa/phone/PhoneManager.java
+++ src/kosa/phone/PhoneManager.java
@@ -1,7 +1,7 @@
 package kosa.phone;
 
-
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class PhoneManager {
     static int phoneCount = 0;
@@ -10,13 +10,17 @@ public class PhoneManager {
 
     public void addPhoneInfo(String name, String phoneNo, String birth) {
 
+/*
         boolean flag = true;
         for (int i = 0; i < phoneCount; i++) {
             if (phoneList.get(i).phoneNo.equals(phoneNo)) {
                 flag = false;
                 break;
             }
-        }
+        }*/
+
+        // 번호가 존재하는지 중복 체크
+        boolean flag = phoneList.stream().noneMatch(info -> info.getPhoneNo().equals(phoneNo));
 
         if (flag) {
             if (10 <= phoneList.size()) {
@@ -26,7 +30,6 @@ public class PhoneManager {
                 phoneList.add(new PhoneInfo(name, phoneNo, birth));
                 phoneCount++;
             }
-
         } else {
             System.out.println("⚠️중복된 번호의 전화번호부가 존재합니다 ⚠️");
             System.out.println();
@@ -35,12 +38,7 @@ public class PhoneManager {
 
     public void listPhoneInfo() {
         if (phoneList.size() > 0) {
-
-            Iterator<PhoneInfo> iter = phoneList.iterator();
-
-            while (iter.hasNext()) {
-                iter.next().show();
-            }
+            phoneList.forEach(PhoneInfo::show);
         } else {
             System.out.println("⚠️현재 전화번호부가 비어있습니다 ⚠️");
             System.out.println();
@@ -48,20 +46,13 @@ public class PhoneManager {
     }
 
     public void searchPhoneInfo(String name) {
-        boolean flag = false;
-        int num = 0;
-        for (int i = 0; i < phoneList.size(); i++) {
 
-            if (phoneList.get(i).name.equals(name)) {
-                flag = true;
-                num = i;
-                break;
-            }
-        }
+        //phoneList 리스트에서 이름이 주어진 이름과 일치하는 첫 번째 PhoneInfo 객체를 찾는 것
+        Optional<PhoneInfo> result = phoneList.stream().filter(info -> info.getName().equals(name)).findFirst();
 
-        if (flag) {
+        if (result.isPresent()) {
             System.out.println(name + "님의 검색 결과");
-            phoneList.get(num).show();
+            result.get().show();
         } else {
             System.out.println("⚠️" + name + "라는 이름의 결과가 존재하지 않습니다 ⚠️");
             System.out.println();
@@ -69,23 +60,15 @@ public class PhoneManager {
     }
 
     public void updatePhoneInfo(String name) {
-        boolean flag = false;
-        int num = 0;
-        for (int i = 0; i < phoneList.size(); i++) {
 
-            if (phoneList.get(i).name.equals(name)) {
-                flag = true;
-                num = i;
-                break;
-            }
-        }
+        Optional<PhoneInfo> result = phoneList.stream().filter(info -> info.getName().equals(name)).findFirst();
 
-        if (flag) {
+        if (result.isPresent()) {
             System.out.println(name + "님의 검색 결과");
-            phoneList.get(num).show();
+            result.get().show();
             System.out.print("수정할 번호 입력 : ");
             String phone = in.nextLine();
-            phoneList.get(num).phoneNo = phone;
+            result.get().setPhoneNo(phone);
         } else {
             System.out.println("⚠️" + name + "라는 이름의 결과가 존재하지 않습니다 ⚠️");
             System.out.println();
@@ -93,50 +76,30 @@ public class PhoneManager {
     }
 
     public void deletePhoneInfo(String name) {
-        boolean flag = false;
-        int num = 0;
-        for (int i = 0; i < phoneList.size(); i++) {
 
-            if (phoneList.get(i).name.equals(name)) {
-                flag = true;
-                num = i;
-                break;
-            }
-        }
+        // phoneList 리스트에서 주어진 이름과 일치하는 모든 PhoneInfo 객체를 제거하는 것
+        boolean removed = phoneList.removeIf(info -> info.getName().equals(name));
 
-        if (flag) {
-            phoneList.remove(phoneList.get(num));
+        if (removed) {
             System.out.println("삭제 되었습니다 !!");
         } else {
             System.out.println("⚠️" + name + "라는 이름의 결과가 존재하지 않습니다 ⚠️");
             System.out.println();
         }
-
     }
 
     public void sortPhoneInfo(int num) {
-        // 이름
         if (num == 1) {
-            Collections.sort(phoneList, (o1, o2) -> o1.getName().compareTo(o2.getName()));
-
-            System.out.println("이름순으로 정렬하였습니다ㅣ!");
-        }
-        // 번호
-        else if (num == 2) {
-
-            Collections.sort(phoneList, (o1, o2) -> o1.getPhoneNo().compareTo(o2.getPhoneNo()));
-
-            System.out.println("번호순으로 정렬하였습니다ㅣ!");
-        }
-        // 생일
-        else if (num == 3) {
-
-            Collections.sort(phoneList, (o1, o2) -> o1.getBirth().compareTo(o2.getBirth()));
-
-            System.out.println("번호순으로 정렬하였습니다ㅣ!");
-        }
-        else {
-            System.out.println("제대로 입력해주세욥");
+            phoneList.sort(Comparator.comparing(PhoneInfo::getName));
+            System.out.println("이름순으로 정렬하였습니다!");
+        } else if (num == 2) {
+            phoneList.sort(Comparator.comparing(PhoneInfo::getPhoneNo));
+            System.out.println("번호순으로 정렬하였습니다!");
+        } else if (num == 3) {
+            phoneList.sort(Comparator.comparing(PhoneInfo::getBirth));
+            System.out.println("생일순으로 정렬하였습니다!");
+        } else {
+            System.out.println("제대로 입력해주세요");
         }
     }
 }
